5c67a0fac41068fea5d4bdfd3fc777af0447fceb
Federico Ferrero <53381800+federico-jf@users.noreply.github.com>
2021-02-12 15:08:06 -0600

    Update Homework_1.R

--- Homework_1.R
+++ Homework_1.R
@@ -1,58 +1,48 @@
-#Homework 1
+# Homework 1
+# PSCI 7381
+# Prof. Dr. Santoro
+# Student: Federico Ferrero
+
+# loading data and libraries
 library(statnet)
 library(UserNetR)
 data("FIFA_Nether")
 
 ?FIFA_Nether
 
-#simple visualization
+# simple visualization
 plot(FIFA_Nether, displaylabels=TRUE)
 
-#network size
+# network size
 network.size(FIFA_Nether)
 summary(FIFA_Nether,print.adj=TRUE)
 
-#density
+# density
 gden(FIFA_Nether)
 
-#number of components (subgroup in which all actors are conected, directely or indirectely)
+# number of components (subgroup in which all actors are conected, directely or indirectely)
+help(components)
 components(FIFA_Nether)
 
-#diameter
+# diameter
 gd <- geodist(FIFA_Nether)
 max(gd$gdist)
 
-#clustering by transitivity
+# clustering by transitivity
 gtrans(FIFA_Nether,mode="graph")
+gtrans(FIFA_Nether,mode="digraph")
 
-#Homework  MORENO DATASET (pag 11)
-install.packages('statnet')
-library(statnet)
-library(UserNetR)
-data(Moreno)
+# degree centrality
 
-#simple visualization
-gender <- Moreno %v% "gender"
-plot(Moreno, vertex.col = gender + 2, vertex.cex = 1.2)
+FIFA_Nether %v% 'vertex.names' #it gives me the names of nodes
+degree(FIFA_Nether) # gmode is set to "digraph" by default.
 
-#network size
-network.size(Moreno)
-summary(Moreno,print.adj=FALSE)
+# closeness centrality
 
-#density
-#calculated by hand
-den_hand <- 2*46/(33*32)
-den_hand
-#calculated with function
-gden(Moreno)
+FIFA_Nether %v% 'vertex.names' #it gives me the names of nodes
+closeness(FIFA_Nether) # gmode is set to "digraph" by default.
 
-#number of components (subgroup in which all actors are conected, directely or indirectely)
-components(Moreno)
+# betweenness centrality
 
-#diameter
-lgc <- component.largest(Moreno,result="graph")
-gd <- geodist(lgc)
-max(gd$gdist)
-
-#clustering by transitivity
-gtrans(Moreno,mode="graph")
+FIFA_Nether %v% 'vertex.names' #it gives me the names of nodes
+betweenness(FIFA_Nether) # gmode is set to "digraph" by default.
