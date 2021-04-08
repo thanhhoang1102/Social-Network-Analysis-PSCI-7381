32b46fb37e0b90f8a0b6518490e33e05e407c536
Federico Ferrero <53381800+federico-jf@users.noreply.github.com>
2021-04-08 15:08:49 -0500

    Update Homework_4.R

--- Homework_4.R
+++ Homework_4.R
@@ -2,7 +2,7 @@
 # Federico Ferrero
 # 4/6/2021
 
-# clear my memory
+# clear your memory
 rm(list=ls())
 
 # Importing edgelist data into R
@@ -46,16 +46,16 @@ plot(network2, layout=layout_with_graphopt)
 # Graphopt layout with smaller arrows
 plot(network2, layout=layout_with_graphopt, edge.arrow.size=.15)
 
-# Changing the color of nodes
+# Changing the color of nodes depending on media type
 col <- c("cadetblue2", "coral2", "goldenrod2")
 V(network2)$color <- col[V(network2)$media.type]
 plot(network2, layout=layout_with_graphopt, edge.arrow.size=.15)
 
 # Changing the shape of nodes based on type of media
-?igraph.vertex.shapes # I have only two different shapes (square and circle). Need a triangle.
-# I will use the code suggested by the documentation.
+?igraph.vertex.shapes # I have only two very different shapes (square and circle,). 
+# I Need a triangle. I will use the code suggested by the documentation.
 
-# first add triangle as a shape
+# first, adding a triangle as a shape
 mytriangle <- function(coords, v=NULL, params) {
   vertex.color <- params("vertex", "color")
   if (length(vertex.color) != 1 && !is.null(v)) {
@@ -73,13 +73,13 @@ mytriangle <- function(coords, v=NULL, params) {
 add_shape("triangle", clip=shapes("circle")$clip,
           plot=mytriangle)
 
-# Now I have three different shapes with igraph. Plot the network with the shapes
+# Now I have three very different shapes with igraph. Plot the network with the shapes
 shape <- c("square", "circle", "triangle")
 V(network2)$shape <- shape[V(network2)$media.type]
 plot(network2, layout=layout_with_graphopt, edge.arrow.size=.3)
 
 # Changing node size using centrality measures
-?degree
+?degree # See documentation
 degree <- degree(network2, mode="in") # all, in, out
 V(network2)$size <- degree*6
 plot(network2, layout=layout_with_graphopt, edge.arrow.size=.3)
@@ -107,13 +107,44 @@ plot(network2, layout=layout_with_graphopt, edge.arrow.size=.2, vertex.label=V(n
      vertex.label.font=35, vertex.label.color="black",
      vertex.label.cex=.7, color.edge=E(network2)$color)
 
-# Adding the legends
+# Adding the legend
+legend(x=-1.9, y=-0.9, c("Newspaper","Television", "Online News"), pch=c(22,21,24),
+       col=col, pt.bg=col,text.font= 8, pt.cex=2, cex=.8, bty="n", ncol=1)
+
+### Making an optimal visualization
+dev.off() # deleting formatting
+
+# Changing the color of nodes depending on media type
+col <- c("cadetblue2", "coral2", "goldenrod2")
+V(network2)$color <- col[V(network2)$media.type]
+plot(network2, layout=layout_with_graphopt, edge.arrow.size=.15)
+
+# Changing node size using indegree centrality 
+degree <- degree(network2, mode="in") # all, in, out
+V(network2)$size <- degree*6
+plot(network2, layout=layout_with_graphopt, edge.arrow.size=.3)
+
+# Labeling nodes with the name of media organization
+plot(network2, layout=layout_with_graphopt, edge.arrow.size=.15, vertex.label=V(network2)$media, 
+     vertex.label.font=35, vertex.label.color="black",
+     vertex.label.cex=.7)
+
+# Changing the size of the edges depending on the weight
+head(edges) # checking variables in edges
+E(network2)$width <- E(network2)$weight/7
+plot(network2, layout=layout_with_graphopt, edge.arrow.size=.15, vertex.label=V(network2)$media, 
+     vertex.label.font=35, vertex.label.color="black",
+     vertex.label.cex=.7, edge.color="gray27")
+
+# Changing the color of the edges depending on the type of tie
+E(network2)$color <- ifelse(E(network2)$type=="hyperlink",'blue','hotpink')
+plot(network2, layout=layout_with_graphopt, edge.arrow.size=.2, vertex.label=V(network2)$media, 
+     vertex.label.font=35, vertex.label.color="black",
+     vertex.label.cex=.8, color.edge=E(network2)$color, main="Making an Optimal Visualization")
+
+# Adding the legenda
 legend(x=-1.9, y=-0.9, c("Newspaper","Television", "Online News"), pch=c(22,21,24),
        col=col, pt.bg=col,text.font= 8, pt.cex=2, cex=.8, bty="n", ncol=1)
 
 legend(x=0.4, y=-0.999999, legend=c("Hyperlink", "Mention"),
        col=c("blue","hotpink"), lty=1, cex=0.8, box.lty=0, text.font= 8)
-
-# deleting formatting
-dev.off()
-
