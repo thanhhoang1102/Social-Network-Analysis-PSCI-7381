09e551b1a736d47878f9f96948f7185d8d97afd8
Federico Ferrero <53381800+federico-jf@users.noreply.github.com>
2021-04-16 16:35:05 -0500

    Update Luke_Chapter_11.R

--- Luke_Chapter_11.R
+++ Luke_Chapter_11.R
@@ -2,8 +2,10 @@
 # Federico Ferrero
 # Luke: Chapter 11: ERGM
 
+library(ggplot2)
 library(UserNetR)
 library(igraph)
+library(statnet)
 data(TCnetworks)
 TCcnt <- TCnetworks$TCcnt
 TCcoll <- TCnetworks$TCcoll
@@ -11,6 +13,7 @@ TCcoll <- TCnetworks$TCcoll
 TCdiss <- TCnetworks$TCdiss
 TCdist <- TCnetworks$TCdist
 summary(TCdiss,print.adj=FALSE)
+
 
 components(TCdiss)
 gden(TCdiss)
@@ -26,3 +29,27 @@ plot(TCdiss,usearrows=FALSE,displaylabels=TRUE,
 legend("bottomleft",legend=c("Local","State",
                              "National"),
        col=2:4,pch=19,pt.cex=1.5)
+
+# setting ERGM (a null model, only the edges term)
+library(ergm)
+DSmod0 <- ergm(TCdiss~edges,
+               control=control.ergm(seed=40))
+class(DSmod0)
+summary(DSmod0)
+
+plogis(coef(DSmod0))
+
+# Including nodes attributes
+scatter.smooth(TCdiss %v% 'tob_yrs',
+               degree(TCdiss,gmode='graph'),
+               xlab='Years of Tobacco Experience',
+               ylab='Degree')
+
+DSmod1 <- ergm(TCdiss ~ edges +
+                 nodefactor('lead_agency') +
+                 nodecov('tob_yrs') ,
+               control=control.ergm(seed=40))
+summary(DSmod1)
+
+# Including dyadic predictors
+
