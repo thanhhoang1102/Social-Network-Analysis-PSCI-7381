81003df9bab43699ae4ee48f11cbea8b73d547c7
Federico Ferrero <53381800+federico-jf@users.noreply.github.com>
2021-04-29 15:54:37 -0500

    Update Homework_4.R

--- Homework_4.R
+++ Homework_4.R
@@ -1,247 +1,151 @@
-# FINAL PROJECT
-# Social Network Analysis
+# HOMEWORK 4
+# Federico Ferrero
+# 4/6/2021
 
 # clear your memory
 rm(list=ls())
 
-# importing edgelist data into R
-library(statnet)
-library(UserNetR)
+# Importing edgelist data into R
 library(igraph)
 
+edges<- read.csv("https://raw.githubusercontent.com/federico-jf/Social-Network-Analysis-PSCI-7381/main/Edgelist-Media-Edges.csv", header=T, as.is=T)
+nodes<- read.csv("https://raw.githubusercontent.com/federico-jf/Social-Network-Analysis-PSCI-7381/main/Edgelist-Media-Nodes.csv", header=T, as.is=T)
 
-edges<- read.csv("https://raw.githubusercontent.com/federico-jf/Social-Network-Analysis-PSCI-7381/main/final_edges_2.csv", header=T, as.is=T)
-nodes<- read.csv("https://raw.githubusercontent.com/federico-jf/Social-Network-Analysis-PSCI-7381/main/final_nodes_2.csv", header=T, as.is=T)
+head(edges) # checking variables in edges
+head(nodes) # checking variables in nodes
 
+# Transforming it into a network object
+network <- graph_from_data_frame(d=edges, vertices=nodes, directed = TRUE)
+network
+summary(network) # asking for summary of network
 
-networkasigraph <- graph_from_data_frame(d=edges, vertices=nodes, directed = TRUE)
-networkasigraph
-class(networkasigraph)
+# Removing the loops
+network2 <- simplify(network, remove.multiple = F, remove.loops = T) 
 
-summary(networkasigraph)
+# Plotting the network
+plot(network2)
 
-# from igraph to statnet
-library(intergraph)
-class(networkasigraph)
+# verify layouts in igraph
+?layout
 
-networkasnet <- asNetwork(networkasigraph)
+# Random layout
+plot(network2, layout=layout_randomly)
 
-class(networkasnet)
-summary(networkasnet)
+# Circular layout
+plot(network2, layout=layout_in_circle)
 
-# size of network
-network.size(networkasnet)
+# Fruchterman-Reingold layout (FR)
+plot(network2, layout=layout_with_fr)
 
-# degree centrality
-network.vertex.names(networkasnet) # it gives me the names of nodes
-degree <-degree(networkasnet, gmode="digraph")
-mean(degree)
+# Kamada and Kawai layout (KK)
+plot(network2, layout=layout_with_kk)
 
-# in degree centrality
-network.vertex.names(networkasnet) # it gives me the names of nodes
-in_degree <-degree(networkasnet, gmode="digraph", cmode="indegree")
+# Graphopt layout
+plot(network2, layout=layout_with_graphopt)
 
-in_degree
-mean(in_degree)
+# Graphopt layout with smaller arrows
+plot(network2, layout=layout_with_graphopt, edge.arrow.size=.15)
 
-# out degree centrality
-mean(nodes$number_tweets)
+# Changing the color of nodes depending on media type
+col <- c("cadetblue2", "coral2", "goldenrod2")
+V(network2)$color <- col[V(network2)$media.type]
+plot(network2, layout=layout_with_graphopt, edge.arrow.size=.15)
 
-# betweenness
-betweenness <-betweenness(networkasnet, gmode="digraph")
-betweenness
+# Changing the shape of nodes based on type of media
+?igraph.vertex.shapes # I have only two very different shapes (square and circle,). 
+# I Need a triangle. I will use the code suggested by the documentation.
 
-#degree distribution graphically
-deg_dis <- as.data.frame(table(degree(networkasnet, gmode="digraph")))
-colnames(deg_dis) <- c('Degree','Frequency')
-plot(deg_dis, main="Degree Distribution #AlevelResults Network", ylim=c(0,400))
+# first, adding a triangle as a shape
+mytriangle <- function(coords, v=NULL, params) {
+  vertex.color <- params("vertex", "color")
+  if (length(vertex.color) != 1 && !is.null(v)) {
+    vertex.color <- vertex.color[v]
+  }
+  vertex.size <- 1/135 * params("vertex", "size")
+  if (length(vertex.size) != 1 && !is.null(v)) {
+    vertex.size <- vertex.size[v]
+  }
+  
+  symbols(x=coords[,1], y=coords[,2], bg=vertex.color,
+          stars=cbind(vertex.size, vertex.size, vertex.size),
+          add=TRUE, inches=FALSE)
+}
+add_shape("triangle", clip=shapes("circle")$clip,
+          plot=mytriangle)
 
-# plotting the network
-# defining labels according degrees
-node_label <- V(networkasigraph)$node_label <- unname(ifelse(degree(networkasigraph)[V(networkasigraph)] > 7, names(V(networkasigraph)), "")) 
-
-# defining color edges depending on type of link (reply = blue / retweet = hotpink)
-edge_color <-E(networkasigraph)$color <- ifelse(E(networkasigraph)$type_link=="reply",'blue','hotpink')
-
+# Now I have three very different shapes with igraph. Plot the network with the shapes
+shape <- c("square", "circle", "triangle")
+V(network2)$shape <- shape[V(network2)$media.type]
+plot(network2, layout=layout_with_graphopt, edge.arrow.size=.3)
 
 # Changing node size using centrality measures
-in_degree1 <- degree(networkasigraph, mode="in") # all, in, out
-#out_degree1 <- degree(networkasigraph, mode="out") # all, in, out
-#in_degree1
-#out_degree1
-#mean(in_degree1)
-#mean(out_degree1)
-in_degree1
-V(networkasigraph)$size <- in_degree1*1.41
+?degree # See documentation
+degree <- degree(network2, mode="in") # all, in, out
+V(network2)$size <- degree*6
+plot(network2, layout=layout_with_graphopt, edge.arrow.size=.3)
 
-V(networkasigraph)[V(networkasigraph)$Profile == "Politician"]$color <- "red"
-V(networkasigraph)[V(networkasigraph)$Profile == "Professor/Researcher"]$color <- "cadetblue"
-V(networkasigraph)[V(networkasigraph)$Profile == "Media organization"]$color <- "goldenrod2"
-V(networkasigraph)[V(networkasigraph)$Profile == "Union Leader"]$color <- "pink"
-V(networkasigraph)[V(networkasigraph)$Profile == "Other"]$color <- "green"
+# Using the audience size value to modify node size
+head(nodes) # checking variables in nodes
+V(network2)$size <- V(network2)$audience.size*0.8
+plot(network2, layout=layout_with_graphopt, edge.arrow.size=.3)
 
+# Labeling nodes with the name of media organization
+plot(network2, layout=layout_with_graphopt, edge.arrow.size=.15, vertex.label=V(network2)$media, 
+     vertex.label.font=35, vertex.label.color="black",
+     vertex.label.cex=.7, edge.color="gray27")
 
-plot(networkasigraph, layout=layout_with_dh,
-     edge.arrow.size=.19,
-     vertex.label.color="black",
-     vertex.label = node_label,
-     vertex.label = node_label,
-     color.edge= edge_color,
-     main="Main relations #AlevelResults discussion")
+# Changing the size of the edges depending on the weight
+head(edges) # checking variables in edges
+E(network2)$width <- E(network2)$weight/7
+plot(network2, layout=layout_with_graphopt, edge.arrow.size=.15, vertex.label=V(network2)$media, 
+     vertex.label.font=35, vertex.label.color="black",
+     vertex.label.cex=.7, edge.color="gray27")
 
-library(RColorBrewer)
-
-summary(networkasigraph)
-
-?layout
- layout_nicely(), 
-layout_with_dh(),
- layout_with_graphopt(), 
- 
-merge_coords(), norm_coords(), normalize()
 # Changing the color of the edges depending on the type of tie
-E(networkasigraph)$color <- ifelse(E(networkasigraph)$type=="reply",'blue','hotpink')
+E(network2)$color <- ifelse(E(network2)$type=="hyperlink",'blue','hotpink')
+plot(network2, layout=layout_with_graphopt, edge.arrow.size=.2, vertex.label=V(network2)$media, 
+     vertex.label.font=35, vertex.label.color="black",
+     vertex.label.cex=.7, color.edge=E(network2)$color)
+
+# Adding the legend
+legend(x=-1.9, y=-0.9, c("Newspaper","Television", "Online News"), pch=c(22,21,24),
+       col=col, pt.bg=col,text.font= 8, pt.cex=2, cex=.8, bty="n", ncol=1)
+
+### Making an optimal visualization
+dev.off() # deleting formatting
+
+# Changing the color of nodes depending on media type
+col <- c("cadetblue2", "coral2", "goldenrod2")
+V(network2)$color <- col[V(network2)$media.type]
+plot(network2, layout=layout_with_graphopt, edge.arrow.size=.15)
+
+# Changing node size using indegree centrality 
+degree <- degree(network2, mode="in") # all, in, out
+V(network2)$size <- degree*6
+plot(network2, layout=layout_with_graphopt, edge.arrow.size=.3)
+
+# Labeling nodes with the name of media organization
+plot(network2, layout=layout_with_graphopt, edge.arrow.size=.15, vertex.label=V(network2)$media, 
+     vertex.label.font=35, vertex.label.color="black",
+     vertex.label.cex=.7)
+
+# Changing the size of the edges depending on the weight
+head(edges) # checking variables in edges
+E(network2)$width <- E(network2)$weight/7
+plot(network2, layout=layout_with_graphopt, edge.arrow.size=.15, vertex.label=V(network2)$media, 
+     vertex.label.font=35, vertex.label.color="black",
+     vertex.label.cex=.7, edge.color="gray27")
+
+# Changing the color of the edges depending on the type of tie
+E(network2)$color <- ifelse(E(network2)$type=="hyperlink",'blue','hotpink')
 plot(network2, layout=layout_with_graphopt, edge.arrow.size=.2, vertex.label=V(network2)$media, 
      vertex.label.font=35, vertex.label.color="black",
      vertex.label.cex=.8, color.edge=E(network2)$color, main="Making an Optimal Visualization")
-?layout
-?plot
-gden(networkasnet)
 
-?gden
-# indegree
-?degree
-degree(network)
-closeness(networkasigraph)
-betweenness(network)
+# Adding the legenda
+legend(x=-1.9, y=-0.9, c("Newspaper","Television", "Online News"), pch=21,
+       col=col, pt.bg=col,text.font= 8, pt.cex=2, cex=.8, bty="n", ncol=1)
 
+legend(x=0.4, y=-0.999999, legend=c("Hyperlink", "Mention"),
+       col=c("blue","hotpink"), lty=1, cex=0.8, box.lty=0, text.font= 8)
 
-degree(network, mode = "in")
-
-in_deg <- network %v% "in_deg" <- degree(network, mode = "in")
-flomarriage # verify if it was added
-# transform network from igraph to network (statnet) and obtain the summary
-library(intergraph)
-class(network)
-
-networkasnet <- asNetwork(network) 
-class(networkasnet)
-summary(networkasnet)
-
-# degree of nodes
-V(network)$media
-degree(network) # gmode is set to "digraph" by default
-
-# betweenness centrality
-betweenness(network) # gmode is set to "digraph" by default.
-
-# plot
-plot(network, edge.arrow.size=.4,vertex.label=V(network)$media)
-
-# ergm
-library(ergm)
-
-# load Florentine marriage data matrix 
-data(flo)
-flo
-
-# create a network object out of the adjacency matrix 
-flomarriage <- network(flo, directed=FALSE)
-flomarriage[,] 
-class(flomarriage)
-
-# create a vector indicating the wealth of each family (in thousands of lira) 
-# and add it to the network object as a covariate
-in_degree <- networkasnet %v% "in_degree" <-c(10,36,27,146,55,44,20,8,42,103,48,49,10,48,32,3)
-wealth
-flomarriage
-
-# load a network object of the Florentine data 
-data(florentine)
-?florentine
-flomarriage
-
-#degree of nodes
-network.vertex.names(flomarriage) # it gives me the names of nodes
-degree(flomarriage)
-
-#degree distribution graphically
-deg_dis <- as.data.frame(table(degree(flomarriage)))
-colnames(deg_dis) <- c('Degree','Frequency')
-plot(deg_dis, main="Degree Distribution Florentine Network")
-
-# calculate eigenvector centrality of each node
-?evcent
-network.vertex.names(flomarriage) # it gives me the names of nodes
-evcent(flomarriage)
-
-# create a visualization of the network where nodes are sized by wealth
-plot(flomarriage, label= network.vertex.names(flomarriage), vertex.cex=wealth/15,
-     main= "Florentine Network by Wealth")
-
-dev.off()
-
-######################### ERGM: a null model
-library(ergm)
-null <- ergm(networkasnet~edges)
-class(null)
-summary(null)
-
-# the null model is constrained by the number of edges in the observed network
-# The probability of an edge being drawn should in theory be the same as density - let's check.
-plogis(coef(null))
-gden(networkasnet)
-
-# add node attribute: wealth
-mod1 <- ergm(networkasnet~edges+nodecov('user_followers_count')+
-               nodecov('user_followed_count')+
-               nodecov('number_tweets')+
-               nodematch('Profile'))
-summary(mod1)
-
-
-# calculate probability of observing tie between families of different incomes
-p_edg <- coef(mod1) [1]
-p_edg
-p_wealth <- coef(mod1) [2]
-p_wealth
-plogis(p_edg + 146*p_wealth + 3*p_wealth) #prob of link between 2 diff families in term of wealth
-
-plogis(p_edg + 146*p_wealth + 103*p_wealth) #prob of link between 2 similar families in term of wealth
-
-
-# calculate betweenness centrality of each node
-network.vertex.names(flomarriage) # it gives me the names of nodes
-betweenness(flomarriage, gmode="graph") # gmode is set to "digraph" by default."digraph" indicates edges 
-#are directed
-
-# add betweenness centrality class as a dyad attribute to the model
-?degree
-in_degree <- networkasnet %v% "in_degree" <-degree(networkasnet)
-flomarriage # verify if it was added
-
-mod2 <- ergm(flomarriage ~ edges+nodecov('wealth')+absdiff("betw"))
-summary(mod2)
-
-# add two measures of local structure to the model
-mod3 <- ergm(flomarriage~edges+nodecov('wealth')++absdiff("betw")+gwesp(0.5, fixed=TRUE)+ triangle)
-summary(mod3)
-
-library('stargazer')
-stargazer(mod3, title = "Testing ERGM model", out="table1.txt")
-
-# Analyzing the model fit
-gof <- gof(mod1)
-# execute goodness of fit function and creates a new object, gof
-
-# produces some gof plots from the object
-par(mfrow=c(2,2))
-plot(gof)
-
-# see gof meassures
-gof
-
-# small p-values indicate problems
-# b/c they indicate that there is a statistically significant difference
-# between observed value of the statistic and the simulated values
-# whereas good fit would have it that these values should be similar.
