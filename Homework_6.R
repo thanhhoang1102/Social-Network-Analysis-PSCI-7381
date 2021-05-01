0d1cb4744e3b35e54759454b60d5f267f9741792
Federico Ferrero <53381800+federico-jf@users.noreply.github.com>
2021-05-01 11:15:16 -0500

    Update Homework_6.R

--- Homework_6.R
+++ Homework_6.R
@@ -91,33 +91,57 @@ legend(x=0.4, y=-1.2, legend=c("Reply/Mentions", "Retweet"),
 
 ################# ERGM model #########################
 library(ergm)
+set.seed(510) # set a random seed so we can produce exactly the same results
 model1 <- ergm(networkasnet ~ edges+ transitiveties,
              control=control.ergm(MCMC.samplesize=500,MCMC.burnin=1000,
                                   MCMLE.maxit=10),verbose=TRUE)
+summary(model1)
+# interpretation model 1
+exp(2.6147278) 
+# 13.66
+# Holding all other effects constant, the relative likelihood of observing 
+# clustering in the network is 13.66.
 
 model2 <- ergm(networkasnet ~ edges+ mutual,
                control=control.ergm(MCMC.samplesize=500,MCMC.burnin=1000,
                                     MCMLE.maxit=10),verbose=TRUE)
 
-model3 <- ergm(networkasnet~edges+nodecov('user_followers_count')+
-                     nodecov('user_followed_count')+
-                     nodecov('number_tweets')+
-                     nodematch('Profile'), control=control.ergm(MCMC.samplesize=500,MCMC.burnin=1000,
-                                                                MCMLE.maxit=10),verbose=TRUE)
+summary(model2)
 
-finalmodel <- ergm(networkasnet ~ edges+ transitiveties+ mutual+ nodecov('user_followers_count')+
-                           nodecov('user_followed_count')+
-                           nodecov('number_tweets')+
-                           nodematch('Profile'),
+model3 <- ergm(networkasnet~edges+nodematch('Profile'), 
+               control=control.ergm(MCMC.samplesize=500,MCMC.burnin=1000,
+                                    MCMLE.maxit=10),verbose=TRUE)
+
+summary(model3)
+# edges captures the number of edges in the model 
+# a homophily term that captures the effect of two actors in the network 
+# having the same Twitter account profile
+# nodematch("Profile")= counts the number of cases in which any two nodes sharing an edge 
+# have that same qualitative attribute (in this case the profile)
+
+# log odds of an edge in the twitter network: 
+# -6.059 X change in edge count + -0.814 x change in count of same profile edges
+
+# log odds of a tie across profiles in the Twitter discussion is -6.059 (edges coefficient)
+# Take the inverse logistic transformation
+# logit^{-1}(-6.059) = I/(1 + exp(6.059)) = 0.0023 = probability of a tie across profiles
+library(gtools)
+a <- inv.logit(-6.059)
+a
+# what about odds in the same level?
+# compute the log odds of such a tie = -6.059 - (-0.814) = -5.245
+# take the same inverse logistic transformation
+# exp(-5.245)/(1 + exp(-5.245)) = 0.0052 = probability of a tie within profiles
+b<- inv.logit(-5.245)
+b
+b-a
+
+# The probability of forming a within-profile edge is 0.0029 higher than forming an across-profile edge
+
+finalmodel <- ergm(networkasnet ~ edges+ transitiveties+ mutual+nodematch('Profile'),
                    control=control.ergm(MCMC.samplesize=500,MCMC.burnin=1000,
                                         MCMLE.maxit=10),verbose=TRUE)
-                   
-summary(model1)
-summary(model2)
-summary(model3)
 summary(finalmodel)
-
-
 library('stargazer')
 stargazer(finalmodel, title = "Testing ERGM model", out="table1.txt")
 
@@ -136,6 +160,13 @@ gof
 # b/c they indicate that there is a statistically significant difference
 # between observed value of the statistic and the simulated values
 # whereas good fit would have it that these values should be similar.
+
+# other ergm
+model3 <- ergm(networkasnet~edges+nodecov('user_followers_count')+
+                       nodecov('user_followed_count')+
+                       nodecov('number_tweets')+
+                       nodematch('Profile'), control=control.ergm(MCMC.samplesize=500,MCMC.burnin=1000,
+                                                                  MCMLE.maxit=10),verbose=TRUE)
 
 
 ##################### working with bigger network
@@ -177,7 +208,15 @@ network_filtered <- get.inducedSubgraph(networkasnet,
                            which(betw > 26.89))#the betweenness media
 
 library(ggplot2)
-gplot(network_filtered,displaylabels=false)
+
+# defining labels according degrees
+node_label <- V(network_filtered)$node_label <- unname(ifelse(betweenness(network_filtered)[V(network_filtered)] > 150000, 
+                                                             names(V(network_filtered)), "")) 
+
+# changing node size using betweenness
+betw
+V(network_filteres)$size <- betw*1.41
+gplot(network_filtered,displaylabels=FALSE)
 
 # filtering by betweenness
 network_filtered <- networkasnet %s% which(betw > 26.89)# the media
@@ -199,12 +238,12 @@ betw
 V(networkasigraph)$size <- betw/20
 
 # plot
-plot(networkasigraph, layout=layout_nicely,
+plot(network_filtered, layout=layout_nicely,
      edge.arrow.size=.19,
      vertex.label.color="black",
      vertex.label = node_label,
      vertex.label = node_label,
-     main="First retweets, replies, and mentions in #AlevelResults discussion")
+     main="TITLE")
 
 
 library(ergm)
